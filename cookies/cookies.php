42f614f852c67ad3d3706cf45d0019f91500687e
David Torres <gsanox@gmail.com>
2025-03-04 17:35:25 +0100

    cookies

---
+++ cookies/cookies.php
@@ -0,0 +1,17 @@
+<?php
+
+// Si tenemos el usuario y contraseña
+if (isset($_POST['user']) && isset($_POST['password'])) {
+    // logica para verificar que el usuario existe etc.
+    setcookie("USUARIO", true, time() + 60, "/questions");
+    /*
+    1. `"usuario"` → Nombre de la cookie.
+    2. `"Juan"` → Valor de la cookie.
+    3. `time() + 3600` → Fecha de expiración (ahora + 3600 segundos = 1 hora).
+    4. `"/"` → Rango de acceso ("/" significa que es accesible en todo el sitio).
+    */
+}
+
+// leemos la cookie si es que la hay
+// $cookie = $_COOKIE["USUARIO"];
+var_dump($_COOKIE["USUARIO"]);
